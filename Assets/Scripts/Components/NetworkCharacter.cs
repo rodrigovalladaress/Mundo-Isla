307e39ca8695aa01399d61a4cb558366bcd7f2d9
dim3trodon <rodriv_tf@hotmail.com>
2014-08-03 17:11:17 +0100

    Movimiento gradual de los jugadores

---
+++ Assets/Scripts/Components/NetworkCharacter.cs
@@ -0,0 +1,40 @@
+﻿// This class smooths the movement of the players in Photon.
+//
+// Version: 1.4
+// Autor: Rodrigo Valladares Santana <rodriv_tf@hotmail.com>
+//
+// http://doc.exitgames.com/en/pun/current/tutorials/tutorial-marco-polo
+using UnityEngine;
+
+public class NetworkCharacter : Photon.MonoBehaviour
+{
+	private Vector3 correctPlayerPos;
+	private Quaternion correctPlayerRot;
+	
+	// Update is called once per frame
+	void Update()
+	{
+		if (!photonView.isMine)
+		{
+			transform.position = Vector3.Lerp(transform.position, this.correctPlayerPos, Time.deltaTime * 5);
+			transform.rotation = Quaternion.Lerp(transform.rotation, this.correctPlayerRot, Time.deltaTime * 5);
+		}
+	}
+	
+	void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+	{
+		if (stream.isWriting)
+		{
+			// We own this player: send the others our data
+			stream.SendNext(transform.position);
+			stream.SendNext(transform.rotation);
+			
+		}
+		else
+		{
+			// Network player, receive data
+			this.correctPlayerPos = (Vector3)stream.ReceiveNext();
+			this.correctPlayerRot = (Quaternion)stream.ReceiveNext();
+		}
+	}
+}
